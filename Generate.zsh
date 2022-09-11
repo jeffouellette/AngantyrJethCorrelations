e400fb12cf43fc12dee1ff5d3c4b092418e0211b
Jeff Ouellette <jeff@Jeffs-MacBook-Pro.local>
2022-09-10 18:03:47 -0600

    idk

--- Generate.zsh
+++ Generate.zsh
@@ -1,134 +1,23 @@
 #! /bin/zsh
 
-source Setup.sh
+declare -a configFiles=("HardQCD_pTHat50_allowRescatter" "HardQCD_pTHat50_allowRescatter_withNPDF")
 
-#bin/gen 1  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job1.root > outputs/pp_gen_job1.out 2> errors/pp_gen_job1.err &
-#bin/gen 2  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job2.root > outputs/pp_gen_job2.out 2> errors/pp_gen_job2.err &
-#bin/gen 3  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job3.root > outputs/pp_gen_job3.out 2> errors/pp_gen_job3.err &
-#bin/gen 4  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job4.root > outputs/pp_gen_job4.out 2> errors/pp_gen_job4.err &
-#
-#wait
-#
-#bin/gen 5  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job5.root > outputs/pp_gen_job5.out 2> errors/pp_gen_job5.err &
-#bin/gen 6  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job6.root > outputs/pp_gen_job6.out 2> errors/pp_gen_job6.err &
-#bin/gen 7  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job7.root > outputs/pp_gen_job7.out 2> errors/pp_gen_job7.err &
-#bin/gen 8  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job8.root > outputs/pp_gen_job8.out 2> errors/pp_gen_job8.err &
-#
-#wait
-#
-#bin/gen 9  2212 2212 5020 50 40000 0 rootFiles/pp/gen_job9.root  > outputs/pp_gen_job9.out 2> errors/pp_gen_job9.err &
-#bin/gen 10 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job10.root > outputs/pp_gen_job10.out 2> errors/pp_gen_job10.err &
-#bin/gen 11 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job11.root > outputs/pp_gen_job11.out 2> errors/pp_gen_job11.err &
-#bin/gen 12 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job12.root > outputs/pp_gen_job12.out 2> errors/pp_gen_job12.err &
-#
-#wait
-#
-#bin/gen 13 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job13.root > outputs/pp_gen_job13.out 2> errors/pp_gen_job13.err &
-#bin/gen 14 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job14.root > outputs/pp_gen_job14.out 2> errors/pp_gen_job14.err &
-#bin/gen 15 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job15.root > outputs/pp_gen_job15.out 2> errors/pp_gen_job15.err &
-#bin/gen 16 2212 2212 5020 50 40000 0 rootFiles/pp/gen_job16.root > outputs/pp_gen_job16.out 2> errors/pp_gen_job16.err &
-#
-#wait
+for configFile in ${configFiles[@]}; do
 
+  #for iJob in $(seq 1 40); do
+  for iJob in $(seq 1 1); do
+  
+    # syntax:      job #    rndm phase  beam A     beam B  sqrt(s)   y*      nevt    config file               root output                                     text output                                     error output
+    cmd="./bin/gen ${iJob}  0           2212       2212    5020      0       1000  aux/${configFile}.config  rootFiles/pp/gen_${configFile}_job${iJob}.root  > outputs/pp_gen_${configFile}_job${iJob}.out   2> errors/pp_gen_${configFile}_job${iJob}.err &"
+    echo "Launching `echo ${cmd} | cut -c1-80`..."
+    eval ${cmd}
+  
+    cmd="./bin/gen ${iJob}  0           1000822080 2212    5020      -0.465  1000  aux/${configFile}.config  rootFiles/pPb/gen_${configFile}_job${iJob}.root > outputs/pPb_gen_${configFile}_job${iJob}.out  2> errors/pPb_gen_${configFile}_job${iJob}.err &"
+    echo "Launching `echo ${cmd} | cut -c1-80`..."
+    eval ${cmd}
 
+    (( iJob % 3 == 0 )) && wait
+  
+  done
 
-
-bin/gen_bkg 51 2212 2212 5020 40000 0 rootFiles/pp/gen_job51.root > outputs/pp_gen_job51.out 2> errors/pp_gen_job51.err &
-bin/gen_bkg 52 2212 2212 5020 40000 0 rootFiles/pp/gen_job52.root > outputs/pp_gen_job52.out 2> errors/pp_gen_job52.err &
-bin/gen_bkg 53 2212 2212 5020 40000 0 rootFiles/pp/gen_job53.root > outputs/pp_gen_job53.out 2> errors/pp_gen_job53.err &
-bin/gen_bkg 54 2212 2212 5020 40000 0 rootFiles/pp/gen_job54.root > outputs/pp_gen_job54.out 2> errors/pp_gen_job54.err &
-
-wait
-
-bin/gen_bkg 55 2212 2212 5020 40000 0 rootFiles/pp/gen_job55.root > outputs/pp_gen_job55.out 2> errors/pp_gen_job55.err &
-bin/gen_bkg 56 2212 2212 5020 40000 0 rootFiles/pp/gen_job56.root > outputs/pp_gen_job56.out 2> errors/pp_gen_job56.err &
-bin/gen_bkg 57 2212 2212 5020 40000 0 rootFiles/pp/gen_job57.root > outputs/pp_gen_job57.out 2> errors/pp_gen_job57.err &
-bin/gen_bkg 58 2212 2212 5020 40000 0 rootFiles/pp/gen_job58.root > outputs/pp_gen_job58.out 2> errors/pp_gen_job58.err &
-
-wait
-
-bin/gen_bkg 59 2212 2212 5020 40000 0 rootFiles/pp/gen_job59.root > outputs/pp_gen_job59.out 2> errors/pp_gen_job59.err &
-bin/gen_bkg 60 2212 2212 5020 40000 0 rootFiles/pp/gen_job60.root > outputs/pp_gen_job60.out 2> errors/pp_gen_job60.err &
-bin/gen_bkg 61 2212 2212 5020 40000 0 rootFiles/pp/gen_job61.root > outputs/pp_gen_job61.out 2> errors/pp_gen_job61.err &
-bin/gen_bkg 62 2212 2212 5020 40000 0 rootFiles/pp/gen_job62.root > outputs/pp_gen_job62.out 2> errors/pp_gen_job62.err &
-
-wait
-
-bin/gen_bkg 63 2212 2212 5020 40000 0 rootFiles/pp/gen_job63.root > outputs/pp_gen_job63.out 2> errors/pp_gen_job63.err &
-bin/gen_bkg 64 2212 2212 5020 40000 0 rootFiles/pp/gen_job64.root > outputs/pp_gen_job64.out 2> errors/pp_gen_job64.err &
-bin/gen_bkg 65 2212 2212 5020 40000 0 rootFiles/pp/gen_job65.root > outputs/pp_gen_job65.out 2> errors/pp_gen_job65.err &
-bin/gen_bkg 66 2212 2212 5020 40000 0 rootFiles/pp/gen_job66.root > outputs/pp_gen_job66.out 2> errors/pp_gen_job66.err &
-
-wait
-
-
-
-
-#bin/gen 101 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job101.root > outputs/pPb_gen_job101.out 2> errors/pPb_gen_job101.err &
-#bin/gen 102 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job102.root > outputs/pPb_gen_job102.out 2> errors/pPb_gen_job102.err &
-#bin/gen 103 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job103.root > outputs/pPb_gen_job103.out 2> errors/pPb_gen_job103.err &
-#bin/gen 104 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job104.root > outputs/pPb_gen_job104.out 2> errors/pPb_gen_job104.err &
-#
-#wait
-#
-#bin/gen 105 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job105.root > outputs/pPb_gen_job105.out 2> errors/pPb_gen_job105.err &
-#bin/gen 106 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job106.root > outputs/pPb_gen_job106.out 2> errors/pPb_gen_job106.err &
-#bin/gen 107 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job107.root > outputs/pPb_gen_job107.out 2> errors/pPb_gen_job107.err &
-#bin/gen 108 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job108.root > outputs/pPb_gen_job108.out 2> errors/pPb_gen_job108.err &
-#
-#wait
-#
-#bin/gen 109 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job109.root > outputs/pPb_gen_job109.out 2> errors/pPb_gen_job109.err &
-#bin/gen 110 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job110.root > outputs/pPb_gen_job110.out 2> errors/pPb_gen_job110.err &
-#bin/gen 111 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job111.root > outputs/pPb_gen_job111.out 2> errors/pPb_gen_job111.err &
-#bin/gen 112 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job112.root > outputs/pPb_gen_job112.out 2> errors/pPb_gen_job112.err &
-#
-#wait
-#
-#bin/gen 113 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job113.root > outputs/pPb_gen_job113.out 2> errors/pPb_gen_job113.err &
-#bin/gen 114 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job114.root > outputs/pPb_gen_job114.out 2> errors/pPb_gen_job114.err &
-#bin/gen 115 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job115.root > outputs/pPb_gen_job115.out 2> errors/pPb_gen_job115.err &
-#bin/gen 116 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_job116.root > outputs/pPb_gen_job116.out 2> errors/pPb_gen_job116.err &
-#
-#wait
-
-
-
-
-bin/gen_bkg 151 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job151.root > outputs/pPb_gen_bkg_job151.out 2> errors/pPb_gen_bkg_job151.err &
-bin/gen_bkg 152 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job152.root > outputs/pPb_gen_bkg_job152.out 2> errors/pPb_gen_bkg_job152.err &
-bin/gen_bkg 153 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job153.root > outputs/pPb_gen_bkg_job153.out 2> errors/pPb_gen_bkg_job153.err &
-bin/gen_bkg 154 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job154.root > outputs/pPb_gen_bkg_job154.out 2> errors/pPb_gen_bkg_job154.err &
-
-wait
-
-bin/gen_bkg 155 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job155.root > outputs/pPb_gen_bkg_job155.out 2> errors/pPb_gen_bkg_job155.err &
-bin/gen_bkg 156 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job156.root > outputs/pPb_gen_bkg_job156.out 2> errors/pPb_gen_bkg_job156.err &
-bin/gen_bkg 157 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job157.root > outputs/pPb_gen_bkg_job157.out 2> errors/pPb_gen_bkg_job157.err &
-bin/gen_bkg 158 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job158.root > outputs/pPb_gen_bkg_job158.out 2> errors/pPb_gen_bkg_job158.err &
-
-wait
-
-bin/gen_bkg 159 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job159.root > outputs/pPb_gen_bkg_job159.out 2> errors/pPb_gen_bkg_job159.err &
-bin/gen_bkg 160 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job160.root > outputs/pPb_gen_bkg_job160.out 2> errors/pPb_gen_bkg_job160.err &
-bin/gen_bkg 161 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job161.root > outputs/pPb_gen_bkg_job161.out 2> errors/pPb_gen_bkg_job161.err &
-bin/gen_bkg 162 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job162.root > outputs/pPb_gen_bkg_job162.out 2> errors/pPb_gen_bkg_job162.err &
-
-wait
-
-bin/gen_bkg 163 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job163.root > outputs/pPb_gen_bkg_job163.out 2> errors/pPb_gen_bkg_job163.err &
-bin/gen_bkg 164 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job164.root > outputs/pPb_gen_bkg_job164.out 2> errors/pPb_gen_bkg_job164.err &
-bin/gen_bkg 165 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job165.root > outputs/pPb_gen_bkg_job165.out 2> errors/pPb_gen_bkg_job165.err &
-bin/gen_bkg 166 2212 1000822080 5020 50 40000 0.465 rootFiles/pPb/gen_bkg_job166.root > outputs/pPb_gen_bkg_job166.out 2> errors/pPb_gen_bkg_job166.err &
-
-wait
-
-
-hadd -f rootFiles/pp/gen_all.root rootFiles/pp/gen_job*.root
-hadd -f rootFiles/pPb/gen_all.root rootFiles/pPb/gen_job*.root
-hadd -f rootFiles/pp/gen_bkg_all.root rootFiles/pp/gen_bkg_job*.root
-hadd -f rootFiles/pPb/gen_bkg_all.root rootFiles/pPb/gen_bkg_job*.root
-rm rootFiles/pp/gen_job*.root
-rm rootFiles/pPb/gen_job*.root
-rm rootFiles/pp/gen_bkg_job*.root
-rm rootFiles/pPb/gen_bkg_job*.root
+done
